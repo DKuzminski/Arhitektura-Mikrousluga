0a1253aa7001a653d033e24d563810fa9a8dc433
User <User@USER-PC>
2017-03-10 01:46:19 +0100

    Edit

--- DiplomskiProject_Client/Global.asax.cs
+++ DiplomskiProject_Client/Global.asax.cs
@@ -14,7 +14,7 @@ namespace DiplomskiProject_Client
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            RouteConfig.RegisterRoutes(RouteTable.Routes); 
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
